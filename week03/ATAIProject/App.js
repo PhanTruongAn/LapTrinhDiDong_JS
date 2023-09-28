dc176804a99085ddf410ad4077ffb11d3e611da8
PhanTruongAn <phanan1000@gmail.com>
2023-09-29 02:38:50 +0700

    Complete 6 Frame

--- week03/ATAIProject/App.js
+++ week03/ATAIProject/App.js
@@ -5,10 +5,16 @@ import {LinearGradient} from 'expo-linear-gradient';
 import Frame1 from "./components/Frame1";
 import Frame2 from "./components/Frame2";
 import Frame3 from "./components/Frame3";
+import Frame4 from "./components/Frame4";
+import Frame5 from "./components/Frame5";
+import Frame6 from "./components/Frame6"
 export default function App() {
   return (
     // <Frame1/>
     // <Frame2/>
-    <Frame3/>
+    // <Frame3/>
+    // <Frame4/>
+    // <Frame5/>
+    <Frame6/>
 )};
 
