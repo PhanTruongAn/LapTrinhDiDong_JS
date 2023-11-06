295d0c1ab4ead0d6ccb5bc0cd28c62077af8af6b
PhanTruongAn <phanan1000@gmail.com>
2023-11-06 15:54:30 +0700

    Week07, Add SignUp

--- week07/component/Screen1.js
+++ week07/component/Screen1.js
@@ -6,12 +6,15 @@ import {
   Image,
   TextInput,
   Pressable,
+  TouchableOpacity,
 } from "react-native";
-import { useEffect,useState } from "react";
-export default function Screen1({navigation}) {
-  var [data,setData] = useState([]);
-  const [userName,setUserName] = useState('');
- 
+import { useEffect, useState } from "react";
+import { useRoute } from "@react-navigation/native";
+export default function Screen1({ navigation }) {
+  var route = useRoute();
+  var [data, setData] = useState([]);
+  const [userName, setUserName] = useState("");
+
   useEffect(() => {
     fetch(`https://65420869f0b8287df1ff5d0a.mockapi.io/Users`)
       .then((response) => response.json())
@@ -19,18 +22,27 @@ export default function Screen1({navigation}) {
         data = json;
         setData(json);
       });
-  },[]);
-  const handleLogin =()=>{
-    const user = data.find(
-      (user) => user.name === userName);
-      if(user){
-        console.log(user)
-            navigation.navigate("Screen2",{userLogin:user});
-      }else{
-        alert("Your name is not exist!")
+  }, []);
+  useEffect(() => {
+    if (route.params?.newData) {
+      setData([...data, route.params.newData]);
+    }
+  }, [route.params?.newData]);
+
+  const handleLogin = () => {
+    if (userName !== "") {
+      const user = data.find((user) => user.name === userName);
+      if (user) {
+        navigation.navigate("Screen2", { userLogin: user });
+      } else {
+        alert("Your name is not exist!");
       }
-  }
-  
+      console.log(user);
+    } else {
+      alert("Please input your name!");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Image
@@ -55,17 +67,50 @@ export default function Screen1({navigation}) {
       <View style={styles.viewEmail}>
         <Image
           source={require("./image/IconEmail.PNG")}
-          style={{ width: 20, height: 20,left:10 }}
+          style={{ width: 20, height: 20, left: 10 }}
         ></Image>
         <TextInput
           placeholder="Enter Your Name"
-          style={{marginLeft:30,width:250,height:43,fontSize:18,fontWeight:400,color:'gray' }}
-          onChangeText={(text) =>setUserName(text)}
+          style={{
+            marginLeft: 30,
+            width: 250,
+            height: 43,
+            fontSize: 18,
+            fontWeight: 400,
+            color: "gray",
+            outline: "none",
+          }}
+          onChangeText={(text) => setUserName(text)}
         ></TextInput>
       </View>
-      <Pressable 
-      onPress={handleLogin}
-      style={styles.btn}><Text style={{alignSelf:"center"}}>Get Started</Text></Pressable>
+      <Pressable onPress={handleLogin} style={styles.btn}>
+        <Text
+          style={{
+            alignSelf: "center",
+            fontWeight: 600,
+            fontSize: 20,
+            color: "white",
+          }}
+        >
+          Get Started
+        </Text>
+      </Pressable>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("SignUp")}
+        style={{ marginTop: 20, alignSelf: "center", alignItems: "center" }}
+      >
+        <Text
+          style={{
+            alignSelf: "center",
+            fontSize: 20,
+            fontWeight: 600,
+            color: "blue",
+          }}
+        >
+          Sign Up
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- week07/component/Screen3.js
+++ week07/component/Screen3.js
@@ -7,44 +7,44 @@ import {
   TextInput,
   Pressable,
 } from "react-native";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useRoute } from "@react-navigation/native";
-export default function Screen3({navigation}) {
-  var [data,setData] = useState([]);
-  const [job,setJob] = useState('');
+export default function Screen3({ navigation }) {
+  var [data, setData] = useState([]);
+  const [job, setJob] = useState("");
   useEffect(() => {
     setData(route.params.insertJob);
-},[]);
-var route = useRoute();
-var handleInsertJob = () => {
-  if (job !== "") {
-    data.jobs.push(job);
-    fetch(`https://65420869f0b8287df1ff5d0a.mockapi.io/Users/${data.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then((updatedUser) => {
-        setData(updatedUser);
-      });
-    navigation.navigate("Screen2");
-  } else {
-    alert("Please input 1 job!");
-  }
-};
+  }, []);
+  var route = useRoute();
+  var handleInsertJob = () => {
+    if (job !== "") {
+      data.jobs.push(job);
+      fetch(`https://65420869f0b8287df1ff5d0a.mockapi.io/Users/${data.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((updatedUser) => {
+          setData(updatedUser);
+        });
+      navigation.navigate("Screen2");
+    } else {
+      alert("Please input 1 job!");
+    }
+  };
 
-console.log(data)
+  console.log(data);
   return (
     <View style={styles.container}>
       <View style={styles.viewAvatar}>
         <Image
-          source={{uri:data.avatar}}
+          source={{ uri: data.avatar }}
           style={{ height: 50, width: 50 }}
         ></Image>
-        <View style={styles.viewTextAvatar}>
+        <View>
           <Text
             style={{
               width: 101,
@@ -58,18 +58,19 @@ console.log(data)
           >
             Hi {data.name}
           </Text>
-          <Text style={{fontWeight:500,top:-12,left:10}}>Have a great day a head</Text>
+          <Text style={{ fontWeight: 500, top: -12, left: 10 }}>
+            Have a great day a head
+          </Text>
         </View>
       </View>
       <Text
         style={{
           width: "80%",
           height: 48,
-          fontFamily: "Epilogue",
           fontSize: 32,
           fontWeight: 700,
           textAlign: "center",
-          marginTop: 30,
+          marginTop: 10,
         }}
       >
         ADD YOUR JOB
@@ -77,19 +78,34 @@ console.log(data)
       <View style={styles.viewInputJob}>
         <Image
           source={require("./image/IconInPutJob.png")}
-          style={{ width: 20, height: 20,left:10 }}
+          style={{ width: 20, height: 20, left: 10 }}
         ></Image>
         <TextInput
-        onChangeText={(text) =>setJob(text)}
+          onChangeText={(text) => setJob(text)}
           placeholder="Input your job"
-          style={{marginLeft:30,width:250,height:43,fontSize:18,fontWeight:400,color:'gray'}}
+          style={{
+            outline: "none",
+            marginLeft: 30,
+            width: 250,
+            height: 43,
+            fontSize: 18,
+            fontWeight: 400,
+            color: "gray",
+            outline: "none",
+          }}
         ></TextInput>
       </View>
-      <Pressable 
-      onPress={handleInsertJob}
-      style={styles.btn}>
-        
-       <Text style={{fontSize:25,fontWeight:600,alignSelf:'center',color:'white'}}>Finish</Text>
+      <Pressable onPress={handleInsertJob} style={styles.btn}>
+        <Text
+          style={{
+            fontSize: 25,
+            fontWeight: 600,
+            alignSelf: "center",
+            color: "white",
+          }}
+        >
+          Finish
+        </Text>
       </Pressable>
       <Image
         source={require("./image/noteandpen.PNG")}
@@ -121,7 +137,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     height: 80,
     width: 100,
-    marginLeft: -150,
+    marginLeft: -80,
   },
   viewInputJob: {
     flexDirection: "row",
@@ -133,5 +149,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     textAlign: "center",
+    margin: 40,
   },
 });
