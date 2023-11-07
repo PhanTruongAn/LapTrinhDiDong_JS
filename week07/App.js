592c1055daa2d19ce00f0d779af2d98c5b5f0dd8
PhanTruongAn <phanan1000@gmail.com>
2023-11-07 14:53:59 +0700

    Week07_Bai3

--- week07/App.js
+++ week07/App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Screen1 from "./component/Screen1";
-import Screen2 from "./component/Screen2";
-import Screen3 from "./component/Screen3";
+// import Screen1 from "./component/Screen1";
+// import Screen2 from "./component/Screen2";
+// import Screen3 from "./component/Screen3";
+import Screen1 from "./component_Bai3/Screen1";
+import Screen2 from "./component_Bai3/Screen2";
+import Screen3 from "./component_Bai3/Screen3";
 import SignUp from "./component/SignUp";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -21,7 +24,8 @@ export default function App() {
         <Stack.Screen name="Screen3" component={Screen3} />
         <Stack.Screen name="SignUp" component={SignUp} />
       </Stack.Navigator>
+      //{" "}
     </NavigationContainer>
-    // <Screen2/>
+    // <Screen1/>
   );
 }
